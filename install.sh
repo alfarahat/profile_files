d632ff13a9465b9a9c09bf9dd3a56d08b165deca
alfarahat <47213731+alfarahat@users.noreply.github.com>
2020-11-22 17:12:14 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -12,8 +12,11 @@ fi
 
 #Install Fonts-Powerline for pretty bars
 sudo apt install fonts-powerline cmatrix curl
-rm -rf ~/.vim
-rm -rf ~/fzf
+if [  -d ~/profile_files ]; then
+	rm -rf ~/profile_files 1>/dev/null 2&>1
+	rm -rf ~/.vim
+        rm -rf ~/fzf
+fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
